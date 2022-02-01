49fb7f22171c4736d1de6852c4e1863b2c51afe9
Joakimblom <joablo90@outlook.com>
2022-02-01 13:15:40 +0100

    Finished P14ForLoops

--- P14ForLoops/Program.cs
+++ P14ForLoops/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+for (int i = 1; i < 1001; i++) 
+{
+    Console.WriteLine(i);
+    
+}
+Thread.Sleep(500);
+for (int i = 200; i > 99; i--) 
+{
+    Console.WriteLine(i);
+}
+Thread.Sleep(500);
+for (int i = 1024; i >= 1; i /= 2 )
+{
+    Console.WriteLine(i);
+}
