b6db9fe878123c20d5772128be65e2ff9e9acea7
Joakimblom <joablo90@outlook.com>
2022-02-08 22:54:26 +0100

    Finished P10 ObjectOrientedProgramming

--- projects/P10ObjectOrientedProgramming/Program.cs
+++ projects/P10ObjectOrientedProgramming/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+string[] names = new string[3];
+for (int num = 0; num < names.Length; num++)
+{
+
+    Console.WriteLine("Give me a name.");
+    
+    if (num == 0)
+    {
+        names[0] = Console.ReadLine();
+    }
+    if (num == 1)
+    {
+        names[1] = Console.ReadLine();
+    }
+     if (num == 2)
+    {
+        names[2] = Console.ReadLine();
+    }
+}
+Console.WriteLine(names[0]+" "+ names[1] +" "+ names[2]);
