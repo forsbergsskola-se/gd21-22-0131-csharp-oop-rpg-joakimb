e6a1eb90c2d8995549bb75f4d6b61347cf22e345
Joakimblom <joablo90@outlook.com>
2022-02-09 09:10:21 +0100

    Finished P2Classes

--- projects/P2Classes/Program.cs
+++ projects/P2Classes/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Person per = new Person();
+Car car = new Car();
+Animal anim = new Animal();
+
+per.talk();
+anim.wild();
+car.gas();
+class Person
+{
+    public void talk()
+    {
+        Console.WriteLine("Person");
+    }
+}
+class Animal
+{
+    public void wild()
+    {
+        Console.WriteLine("Animal");
+    }
+}
+class Car
+{
+    public void gas()
+    {
+        Console.WriteLine("Car");
+    }
+}
